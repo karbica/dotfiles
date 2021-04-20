26152d36b239bf7d9d130712e5df152ff4933f7e
Kevin Arbi <karbica@eml.cc>
2021-04-19 19:37:34 -0700

    good second pass of init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,22 +3,22 @@ local g = vim.g
 local fn = vim.fn
 local cmd = vim.cmd
 local scopes = {
-    o = vim.o,  -- nvim global options table
-    b = vim.bo, -- nvim buffer options table
-    w = vim.wo  -- nvim window options table
+	o = vim.o,  -- nvim global options table
+	b = vim.bo, -- nvim buffer options table
+	w = vim.wo  -- nvim window options table
 }
 
 -- convenient option setter
 local function opt(scope, key, value)
-    scopes[scope][key] = value
-    if scope ~= 'o' then scopes['o'][key] = value end
+	scopes[scope][key] = value
+	if scope ~= 'o' then scopes['o'][key] = value end
 end
 
 -- convenient keymap setter
 local function map(mode, lhs, rhs, opts)
-    local options = { noremap = true }
-    if opts then options = vim.tbl_extend('force', options, opts) end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+	local options = { noremap = true }
+	if opts then options = vim.tbl_extend('force', options, opts) end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 -- PLUGINS
@@ -33,13 +33,16 @@ paq 'justinmk/vim-sneak'
 paq 'nvim-lua/popup.nvim'
 paq 'nvim-lua/plenary.nvim'
 paq 'nvim-telescope/telescope.nvim'
+paq 'nvim-treesitter/nvim-treesitter'
+
+-- COLORSCHEME
+g["ayucolor"] = 'dark'
+cmd 'colorscheme quantum'
 
 -- OPTIONS
 g.mapleader = " "
 local indent = 2
-g["ayucolor"] = 'dark'
-cmd 'colorscheme quantum'
-opt('b', 'expandtab', true)
+opt('b', 'expandtab', false)
 opt('b', 'smartindent', true)
 opt('b', 'shiftwidth', indent)
 opt('b', 'tabstop', indent)
@@ -47,12 +50,16 @@ opt('w', 'list', true)
 opt('w', 'number', true)
 opt('w', 'relativenumber', true)
 opt('w', 'wrap', false)
+opt('w', 'signcolumn', 'yes')
+opt('w', 'listchars', 'tab:⟶ ,nbsp:␣,trail:‧,eol:↯,precedes:«,extends:»')
 opt('o', 'termguicolors', true)
 opt('o', 'hidden', true)
 opt('o', 'wildmode', 'list:longest')
 opt('o', 'ignorecase', true)
+opt('o', 'smartcase', true)
 opt('o', 'splitright', true)
 opt('o', 'splitbelow', true)
+opt('o', 'shiftround', true)
 opt('o', 'scrolloff', 4)
 opt('o', 'sidescrolloff', 8)
 
@@ -64,26 +71,30 @@ map('n', '<leader>vs', '<C-W>v<C-W>l<CR>')
 map('n', '<leader>ff', '<cmd>lua require(\'telescope.builtin\').find_files()<CR>')
 -- map('n', '<leader>fg', '<cmd>lua require(\'telescope.builtin\').live_grep()<CR>')
 
+-- TREESITTER
+local ts = require('nvim-treesitter.configs')
+ts.setup { ensure_installed = 'maintained', hightlight = {enable = true} }
+
 -- LUALINE
-require('lualine').setup{
-  options = {
-    section_separators = {'🮧', '🭋'},
-    component_separators = {'', ''}
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  }
+require('lualine').setup {
+	options = {
+		section_separators = {'🮧', '🮧'},
+		component_separators = {'', ''}
+	},
+	sections = {
+		lualine_a = {'mode'},
+		lualine_b = {'branch'},
+		lualine_c = {'filename'},
+		lualine_x = {'encoding', 'filetype'},
+		lualine_y = {'progress'},
+		lualine_z = {'location'}
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = {'filename'},
+		lualine_x = {'location'},
+		lualine_y = {},
+		lualine_z = {}
+	}
 }
