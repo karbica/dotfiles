dd24c55c81e6a946fe49b1b53601bf208a286bef
Kevin Arbi <karbica@eml.cc>
2023-02-25 13:00:54 -0800

    chore: check if vscode for lua nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,15 @@
-require('settings')
-require('keymaps')
-require('colorscheme')
-require('plugins/packer')
-require('plugins/nvim-treesitter')
-require('plugins/telescope')
-require('plugins/gitsigns')
-require('plugins/indent-blankline')
-require('plugins/wilder')
-require('plugins/lualine')
-require('plugins/nvim-autopairs')
+if vim.g.vscode then
+  -- VSCode Extension
+else
+  require('settings')
+  require('keymaps')
+  require('colorscheme')
+  require('plugins/packer')
+  require('plugins/nvim-treesitter')
+  require('plugins/telescope')
+  require('plugins/gitsigns')
+  require('plugins/indent-blankline')
+  require('plugins/wilder')
+  require('plugins/lualine')
+  require('plugins/nvim-autopairs')
+end
